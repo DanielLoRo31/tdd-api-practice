61cd0c46efd0642b41bd788725966e71263733d5
DanielLoRo31 <daniel.lozano.rodriguez2000@gmail.com>
2022-04-26 14:43:15 -0500

    add: mocha test structure

---
+++ test/GreetBuilder.spec.js
@@ -0,0 +1,7 @@
+describe('GreetBuilder class test', () => {
+  it('should greet a person', async () => {});
+
+  it('should throw NameInvalidException if name is "Jorge"', async () => {});
+
+  it('should thrown NameIsUndefinedException if no name provided', async () => {});
+});
